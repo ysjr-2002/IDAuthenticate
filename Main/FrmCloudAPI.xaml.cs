951ec7f7c98386bc5694b1d69bcf479402abc1e1
ysj <ysjr-2002@163.com>
2016-09-20 18:45:02 +0800

    ok

--- Main/FrmCloudAPI.xaml.cs
+++ Main/FrmCloudAPI.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Main.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,6 +25,7 @@ namespace Main
     /// </summary>
     public partial class FrmCloudAPI : Window
     {
+        const string URL = "http://60.205.107.219";
         const string username = "aobidao";
         const string password = "abd123";
         public FrmCloudAPI()
@@ -28,20 +33,18 @@ namespace Main
             InitializeComponent();
         }
 
+
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
             var data = "username=" + username + "&password=" + password;
             var buffer = System.Text.Encoding.UTF8.GetBytes(data);
 
-            var url = string.Concat("https://v2.koalacam.net/auth/login", "");
+            var url = string.Concat(URL, "/auth/login");
 
+            Stopwatch sw = Stopwatch.StartNew();
             HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
             wr.ContentType = "application/x-www-form-urlencoded";
             wr.Method = "POST";
-            wr.KeepAlive = true;
-            wr.UserAgent= "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0;)";
-            wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
-            wr.UserAgent = "Koala Admin";
             wr.ContentLength = buffer.Length;
 
             var requeststream = wr.GetRequestStream();
@@ -53,26 +56,55 @@ namespace Main
             StreamReader sr = new StreamReader(stream, System.Text.Encoding.UTF8);
             var json = sr.ReadToEnd();
             Console.WriteLine(json);
+            sw.Stop();
+            Console.WriteLine("耗时:" + sw.ElapsedMilliseconds);
 
-            var cookies = response.Cookies;
-            foreach (Cookie cookie in cookies)
-            {
-                Console.WriteLine(cookie);
-            }
+            var headers = response.Headers;
+            //foreach (var key in headers.AllKeys)
+            //{
+            //    var val = headers[key];
+            //    Console.WriteLine(string.Format("key={0};value={1}", key, val));
+
+            //    if (key.Contains("Set-Cookie"))
+            //    {
+            //        cookie = val;
+            //        Console.WriteLine("break");
+            //        break;
+            //    }
+            //}
+
+
+            cookie = headers["Set-Cookie"];
+            Console.WriteLine("cookie:" + cookie);
+
+            JavaScriptSerializer serialize = new JavaScriptSerializer();
+            var obj = serialize.Deserialize<JsonLogin>(json);
         }
+
+        string cookie = "";
 
         private void btnGetStatus_Click(object sender, RoutedEventArgs e)
         {
-            var url = string.Concat("https://60.205.107.219/auth/status", "");
-
+            Stopwatch sw = Stopwatch.StartNew();
+            var url = string.Concat(URL, "/auth/status");
             HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
-            wr.Method = "GET";
-            //request.ContentType = "application/x-www-form-urlencoded";
-            wr.Accept = "*/*";
-            wr.Timeout = 150000;
-            wr.AllowAutoRedirect = false;
-
+            wr.Method = "Get";
+            wr.Headers["cookie"] = cookie;
             var response = wr.GetResponse();
+            var stream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(stream, System.Text.Encoding.UTF8);
+            var json = sr.ReadToEnd();
+
+            sr.Close();
+            response.Close();
+
+            sw.Stop();
+            Console.WriteLine("耗时:" + sw.ElapsedMilliseconds);
+
+            Console.WriteLine(json);
+
+            JavaScriptSerializer serialize = new JavaScriptSerializer();
+            var obj = serialize.Deserialize<JsonStatus>(json);
         }
 
         private void btnComapre_Click(object sender, RoutedEventArgs e)
@@ -105,6 +137,28 @@ namespace Main
             {
                 Console.WriteLine(cookie);
             }
+        }
+
+        private void btnUnixDT_Click(object sender, RoutedEventArgs e)
+        {
+            var dt = ConvertIntDateTime(1477843200);
+            Console.WriteLine(dt.ToString());
+        }
+
+        public static System.DateTime ConvertIntDateTime(double d)
+        {
+            System.DateTime time = System.DateTime.MinValue;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            time = startTime.AddSeconds(d);
+            return time;
+        }
+
+        public static double ConvertDateTimeInt(System.DateTime time)
+        {
+            double intResult = 0;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            intResult = (time - startTime).TotalSeconds;
+            return intResult;
         }
     }
 }
---
+++ Main/ViewModel/JsonLogin.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.ViewModel
+{
+    class JsonLogin
+    {
+        public string code { get; set; }
+
+        public JsonLoginData data { get; set; }
+    }
+
+    class JsonLoginData
+    {
+        public string company { get; set; }
+
+        public string contact { get; set; }
+
+        public string email { get; set; }
+
+        public int id { get; set; }
+
+        public bool password_reseted { get; set; }
+
+        public string phone { get; set; }
+
+        public int role_id { get; set; }
+
+        public string username { get; set; }
+    }
+
+
+
+    class JsonStatus
+    {
+        public string code { get; set; }
+
+        public JsonStatusData data { get; set; }
+
+        public bool password_reseted { get; set; }
+
+        public string phone { get; set; }
+
+        public int role_id { get; set; }
+
+        public string username { get; set; }
+    }
+
+
+    class JsonStatusData
+    {
+        public string company { get; set; }
+
+        public string contact { get; set; }
+
+        public string email { get; set; }
+
+        public int id { get; set; }
+
+        public limitation limitation { get; set; }
+    }
+
+
+    class limitation
+    {
+        public int called { get; set; }
+
+        public double expiration { get; set; }
+
+        public int id { get; set; }
+
+        public int quota { get; set; }
+
+        public int user_id { get; set; }
+    }
+}
